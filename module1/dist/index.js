26d7b0755ef34f7e397bee4a8d84a7a7a2ac1c0b
Barik <barik@Bariks-MacBook-Air.local>
2024-10-31 08:46:35 +0600

    added few variable

--- module1/dist/index.js
+++ module1/dist/index.js
@@ -1,1 +1,5 @@
 "use strict";
+let age = 26;
+let newName = "Md. A. Barik";
+let myRoll = "17sta051";
+console.log(age, myname, myRoll);
---
+++ module1/src/index.ts
@@ -0,0 +1,5 @@
+let age : number = 26;
+let newName : string = "Md. A. Barik";
+let myRoll : string = "17sta051";
+
+console.log(age, myname, myRoll);
