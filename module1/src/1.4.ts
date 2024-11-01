01785195a6933d80f1b961c90e6c69272fcde1cf
mdabarik <mdabarik19@gmail.com>
2024-11-01 11:55:29 +0600

    basic variable example added

---
+++ module1/src/1.4.ts
@@ -0,0 +1,31 @@
+// basic data type
+
+// string 
+let firstName : string = "barik";
+
+// number
+let roll: number = 123;
+
+// boolean
+let isAdmin : boolean = true;
+
+// undefined
+let x : undefined = undefined;
+
+// null
+let y : null = null;
+
+
+// anytype if not value is defined (don't use it)
+let d;
+d = 123;
+d = "ear";
+d = true;
+
+// array
+let friends = ["barik", "monir", "don"];
+let eligibleRollList : number[] = [1, 3]
+
+// tuple --> array --> order --> type of values 
+let coordinates : [number, number] = [1, 5];
+let ageName : [number, string, boolean] = [26, "barik", true];
