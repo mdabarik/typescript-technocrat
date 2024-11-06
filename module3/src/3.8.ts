422981cdcf4d5626abaa52574fa5c57328f9aec0
mdabarik <mdabarik19@gmail.com>
2024-11-06 10:10:39 +0600

    polymorphism

---
+++ module3/src/3.8.ts
@@ -0,0 +1,77 @@
+{
+    // polymorphism
+    class Person {
+        getSleep() {
+            console.log('I am sleeping 8 hours per day');
+        }
+    }
+
+    class Student extends Person {
+        getSleep() {
+            console.log('I am sleeping 7 hours per day');
+        }
+    }
+
+    class Developer extends Person {
+        getSleep() {
+            console.log('I am sleeping 5 hours per day');
+        }
+    }
+
+    const getSleepingHours = (param: Person) => {
+        param.getSleep();
+    }
+
+    const person1 = new Person();
+    const person2 = new Student();
+    const person3 = new Developer();
+
+    getSleepingHours(person1);
+    getSleepingHours(person2);
+    getSleepingHours(person3);
+
+    class Shape {
+        getArea() : number {
+            return 0;
+        }
+    }
+
+
+    class Circle extends Shape {
+        radius: number;
+        constructor(raidus: number) {
+            super();
+            this.radius = raidus;
+        }
+        getArea(): number {
+            return Math.PI * this.radius * this.radius;
+        }
+    }
+
+    class Rectangle extends Shape {
+        height: number;
+        width: number;
+        constructor(height: number, width: number) {
+            super();
+            this.height = height;
+            this.width = width;
+        }
+        getArea(): number {
+            return this.height * this.width;
+        }
+    }
+
+    const getShapeArea = (param : Shape) => {
+        console.log(param.getArea());
+    }
+
+    const shape1 = new Shape();
+    const shape2 = new Circle(5);
+    const shape3 = new Rectangle(5,7);
+
+    getShapeArea(shape1);
+    getShapeArea(shape2);
+    getShapeArea(shape3);
+
+    //
+}
