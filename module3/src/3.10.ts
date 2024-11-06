caf80538c7a6cb88fcb6fd419cafce053bc3c97d
mdabarik <mdabarik19@gmail.com>
2024-11-06 10:44:22 +0600

    encapsulation

---
+++ module3/src/3.10.ts
@@ -0,0 +1,40 @@
+{
+    // access modifiers - encapsulated
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        private _balance: number;
+
+        constructor(id: number, name: string, _balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = _balance;
+        }
+
+        public addDeposit(amount: number) {
+            this._balance = this._balance + amount;
+        }
+
+        public getBalance() {
+            return this._balance;
+        }
+    }
+
+    class StudentAccount extends BankAccount {
+        test() {
+            this.name;
+        }
+    }
+
+    const poorManAcnt = new BankAccount(111, "Mr. A", 20);
+    poorManAcnt.addDeposit(300);
+    const myBalance = poorManAcnt.getBalance();
+    console.log(myBalance);
+
+
+
+
+
+
+    //
+}
